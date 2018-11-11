0174c12a592ded3ffd1f4a68d7fd6f2fd58b9f72
Guangrui Li <lgr2819@outlook.com>
2018-11-11 12:05:12 +0800

    Update InterpretabelNN.py

--- InterpretabelNN.py
+++ InterpretabelNN.py
@@ -12,7 +12,7 @@ qu_feat = feat_file['query_f']
 attr = attr_file['attribute']
 a_key = attr_file['key']
 
-#attr[0,:]=0
+
 attr = attr[1:,:]
 ga_label = feat_file['gallery_label'][0]
 qu_label = feat_file['query_label'][0]
@@ -33,50 +33,81 @@ for s in range(len(ga_label)):
     if cu_label not in dic_label:
         dic_label[cu_label] = len(dic_label)
 
-#np.save('ga_feat.npy', ga_feat)
-#np.save('attr_img.npy', attr_img)
-ga_feat = np.load('ga_feat.npy')
-attr_img = np.load('attr_img.npy')
+washed_index =np.where(ga_label!=-1)
+ga_feat = np.squeeze(ga_feat[washed_index,:])
+
+
+attr_img = np.zeros((num_attr,num_img), dtype=float)
+#attr_img = np.zeros(num_attr, dtype=float)
+
+for i in range(num_img):
+    label = ga_label[i]
+
+    if label==-1:
+        continue
+    else:
+        cu_label = dic_label[label]
+        attr_img[:,i] = attr[:,cu_label]
+            #attr_img = np.concatenate((attr_img,attr[:,cu_label]), axis=1)
+
+attr_img = np.squeeze(attr_img[:,washed_index])
+print('junk sample cleaned', attr_img.shape, ga_feat.shape)
 num_img = ga_feat.shape[0]
 num_attr  = attr.shape[0]
 num_dim = ga_feat.shape[1]
 
-attr_img_in = 1 - attr_img # in means inverse
 
 
 count_by_attr = np.sum(attr_img,axis=1, keepdims=True)
+ratio_for_attr = 1 - count_by_attr/num_img
+mean_ratio = int(np.mean(ratio_for_attr)*100)
+print('average ratio for attribute in images:', mean_ratio)
 count_by_attr = np.repeat(count_by_attr,num_dim, axis=1)
-count_by_attr_in = np.sum(attr_img_in,axis=1, keepdims=True)
-count_by_attr_in = np.repeat(count_by_attr_in,num_dim, axis=1)
-#print(count_by_attr)
-print(a_key)
-#print(ga_feat.shape)
-#attr = torch.Tensor(attr_img).float()
-#g_feat = torch.Tensor(ga_feat)
 
-percen = np.percentile(ga_feat,90, axis=1,keepdims=True, interpolation='higher',)
-#print(percen[0])
+count_by_unit = np.sum(ga_feat,axis=0, keepdims=True)
+count_by_unit = np.repeat(count_by_unit,num_attr, axis=0)
+
+
+'''
+percen = np.percentile(ga_feat,90, axis=1,keepdims=True, interpolation='higher')
 for i in range(ga_feat.shape[0]):
   #  print(np.where(ga_feat[i,:]>=percen[i]))
-    ga_feat[i,np.where(ga_feat[i,:]>=percen[i])]=0.5
+    ga_feat[i,np.where(ga_feat[i,:]>=percen[i])]=1
     ga_feat[i,np.where(ga_feat[i,:]<percen[i])]=0
+'''
+percen = np.percentile(ga_feat,mean_ratio, axis=0, interpolation='higher')
+for i in range(num_dim):
+    ga_feat[np.where(ga_feat[:,i] >=percen[i]),i]=1
+    ga_feat[np.where(ga_feat[:,i] < percen[i]),i]=0
 
-print(np.sum(ga_feat, axis=1)[:10])
 
-score = np.divide(np.matmul(attr_img, ga_feat), count_by_attr)
-score_in = np.divide(np.matmul(attr_img_in, ga_feat), count_by_attr_in)
+print(np.sum(ga_feat, axis=0)[:10])
+
+relation = np.matmul(attr_img, ga_feat)
+score = np.divide(relation, count_by_attr)
 #score = score-score_in
-location = np.argmax(score)
-print(score.shape)
-dim1 = int(location/num_dim),
+score2 = np.divide(relation, count_by_unit)
+
+
+
+location = np.argmax(score2)
+dim1 = int(location/num_dim)
 dim2 =location%num_dim - 1
-print(location, dim1, dim2)
+
+print('Attribute:', a_key[dim1], ' Unit:', dim2)
+
+max_score = score[dim1][dim2]
+max_score2 = score2[dim1][dim2]
+
+print(max_score*100,'% of attributes')
+print(max_score2*100,'% of units')
+
 ###################################################
 #Visualization
 ###################################################
 
 tov_attr  = np.squeeze(attr_img[dim1,:])
-tov_unit = np.squeeze(ga_feat[:,dim2])
+tov_unit = np.squeeze(ga_feat[:,dim2])*0.5
 #tov_norm = LA.norm(tov_unit, ord=2)
 #tov_unit = tov_unit/tov_norm
 #tov_unit*=6
@@ -89,4 +120,7 @@ ax.bar(axis_x, tov_attr, label='attribute', color='g', alpha=0.5)
 ax.bar(axis_x, tov_unit, label='unit', color='r', alpha=0.5)
 plt.legend(loc = 'upper right')
 plt.show()
+
+
+
 
